885d3cd66b10111649a91a1c486a0669e2ea83a9
Joao Reis <joao097reis@gmail.com>
2026-02-19 16:39:38 -0300

    Estrutura da página de estoque
    Criação de todo o layout e estrutura de  funcionalidade do HTML da página de controle de estoque. Não furam adicionados nenhuma funcionalidade, somente a adição da parte visual e estrutural. Aguardar próximas atualizações

---
+++ public/js/controle_estoque.js
@@ -0,0 +1,4 @@
+document.getElementById('codi_show').addEventListener('click', function() {
+    const info = document.getElementById('codigo');
+    info.style.display = info.style.display === 'none' ? 'block' : 'none';
+});
--- public/js/dashboard-adm.js
+++ public/js/dashboard-adm.js
@@ -143,7 +143,7 @@ class preview {
                     evt.preventDefault();
                     evt.stopPropagation();
                     preview.innerHTML = 
-                        `<h2>Vendas realizadas últimas 24h: para <a href="estoque">Informações detalhadas</a></h2>
+                        `<h2>Vendas realizadas últimas 24h: para <a href="relatorio">Informações detalhadas</a></h2>
                         <h4>Total de vendas: <strong>${dados.dados.qtd_vendas}</strong></h4>
                         <h4>Quantidade total de produtos vendidos: <strong>${dados.dados.qtd_itens_total}</strong></h4>`
             })
